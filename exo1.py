c24a4cd5485dae0f26f4ed9f3d28ac41cd37acd9
daniel <danielhomelema22@gmal.com>
2026-06-16 14:43:30 +0100

    ajout du fichier config

--- exo1.py
+++ exo1.py
@@ -1,1 +1,21 @@
-print("hello word exo1")
+import random
+
+print("Hi! Welcome to the Number Guessing Game.\nYou have 7 chances to guess the number. Let's start!")
+
+low = int(input("Enter the Lower Bound: "))
+high = int(input("Enter the Upper Bound: "))
+
+print(f"\nYou have 7 chances to guess the number between {low} and {high}. Let's start!")
+
+num = random.randint(low, high) 
+# Total allowed chances
+ch = 7 
+# Guess counter
+gc = 0                         
+
+while gc < ch:
+    gc += 1
+    guess = int(input('Enter your guess: '))
+ if guess == num:
+        print(f'Correct! The number is {num}. You guessed it in {gc} attempts.')
+        break
